c95e7097553adeae258e8d71b5adc41674bbcce4
Bensoli <benswambua96@gmail.com>
2024-07-10 18:35:26 +0300

    creating server with http verses with express. Familiarized with all http verbs: get delete post all put use

--- app.js
+++ app.js
@@ -1,8 +1,30 @@
-const http = require('http')
+const express = require('express'); //importing the module
+const app = express() //invoking it; creating an instance
 
-const server = http.createServer((req,res)=>{
-    console.log('user hit the server')
-    res.end('home page')
+//express itself is an objact with the following method:
+//app.get -READ DATA
+//app.post -INSERT DATA
+//app.delete -DELETE DATA
+//app.all
+//app.use -MIDDLEWARE
+//app.put -UPDATE DATA
+//app.listen -LISTENING TO ALL CLIENT REQUESTS
+
+app.get('/', (req,res)=>{  //path to resource user is trying to access, callback function invoked every time user performs a get request
+    console.log('user hit home resource');
+    res.send('Home Page') // the callback takes in 2 arguments req,res. for response express uses res and the .send method to return data to client
+} )
+
+app.get('/about', (req,res)=>{
+    console.log('user hit about resource')
+    res.send('About page')
 })
 
-server.listen(5000)
+app.all('*',(req,res)=>{
+    res.send('<h1>Resource not found<h1>')
+    console.log('User accessing unavailable page');
+})
+
+app.listen(5000, ()=>{
+    console.log('server is listening on port 5000...')
+})
