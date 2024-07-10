850eac8318cd228e42db200f2e08f21cbe314191
Bensoli <benswambua96@gmail.com>
2024-07-10 14:51:03 +0300

    .createServer and .listen methods of the http module

--- app.js
+++ app.js
@@ -1,1 +1,8 @@
-console.log('Expresss Tutorial')
+const http = require('http')
+
+const server = http.createServer((req,res)=>{
+    console.log('user hit the server')
+    res.end('home page')
+})
+
+server.listen(5000)
